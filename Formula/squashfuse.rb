d058fc72567cb6a580cee333eb0d73035c450003
Stephen Levine <stephen.levine@gmail.com>
2022-07-24 22:59:01 -0400

    Create squashfuse.rb

---
+++ Formula/squashfuse.rb
@@ -0,0 +1,32 @@
+class Squashfuse < Formula
+  desc "FUSE filesystem to mount squashfs archives"
+  homepage "https://github.com/vasi/squashfuse"
+  url "https://github.com/vasi/squashfuse/releases/download/0.1.105/squashfuse-0.1.105.tar.gz"
+  sha256 "3f776892ab2044ecca417be348e482fee2839db75e35d165b53737cb8153ab1e"
+  license "BSD-2-Clause"
+
+  depends_on "pkg-config" => :build
+  depends_on "lz4"
+  depends_on "lzo"
+  depends_on "squashfs"
+  depends_on "xz"
+  depends_on "zstd"
+
+  on_linux do
+    depends_on "libfuse"
+  end
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    # Unfortunately, making/testing a squash mount requires sudo privileges, so
+    # just test that squashfuse execs for now.
+    output = shell_output("#{bin}/squashfuse --version 2>&1", 254)
+    assert_match version.to_s, output
+  end
+end
